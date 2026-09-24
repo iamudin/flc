99ec984c82e43d48caf190952a8407494a2186ed
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-09-24 16:55:33 +0700

    feat: add files view template with data table and pagination

--- src/views/files.blade.php
+++ src/views/files.blade.php
@@ -20,7 +20,7 @@
                 <td width="70px">
                     <div class="btn-group">
                     <button data-copy="{{ (request()->secure() ? 'https://' : 'http://') . $item->host . '/media/' . $item->file_name }}" class="copy btn btn-sm btn-warning"><i class=" fa fa-link "></i></button>
-                    <span data-media="{{ (request()->secure() ? 'https://' : 'http://') . $item->host . '/media/' . $item->file_name }}" data-ext="{{ str(media_extension($item->file_name))->lower() }}" class="btn-view-media btn btn-sm btn-primary "><i class="fa fa-eye"></i></span>
+                    <span data-media="{{ (request()->secure() ? 'https://' : 'http://') . $item->host . '/media/' . $item->file_name }}" data-ext="{{ str(get_ext($item->file_name))->lower() }}" class="btn-view-media btn btn-sm btn-primary "><i class="fa fa-eye"></i></span>
                     <button onclick="lw_media_destroy('{{ $item->file_name }}')" href="" class="btn btn-sm btn-danger fa fa-trash-o"></button>
                 </div>
                 </td>
